8e1806611645b52ee601a5f248856e02d1247662
AhmadShahbalayev <ahmedsahbalayev@gmail.com>
2020-04-30 16:35:38 +0400

    +

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import StockList from './content/stock/stock-list/StockList';
+
+class App extends React.Component {
+  render() {
+    return <StockList />
+  }
+}
+
+export default App;
