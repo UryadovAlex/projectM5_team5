a606208800028047883cc4610fc2aad5daa1c427
1nsideR-1337 <eln.ismayilov@gmail.com>
2020-04-30 06:14:28 -0700

    'updateByElnar'

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+class App extends React.Component{
+    
+    render(){
+        return(
+            
+
+
+
+<p>as</p>
+        )
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import Buy from './components/content/buy/Buy'
 
 ReactDOM.render(
-    <App />,
+    <Buy />,
     document.querySelector('#root')
 );
