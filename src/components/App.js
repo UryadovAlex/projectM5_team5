19a188b9d9a258d37819ff264dc2c99e3515eccd
Mahammad Zeynalov <magamed9r2.2011@gmail.com>
2020-04-30 16:57:56 +0400

    mahammad push

--- src/components/App.js
+++ src/components/App.js
@@ -4,56 +4,17 @@ import { BrowserRouter, Route } from "react-router-dom";
 // import styles from './App.module.css';
 import Header from './header/Header';
 import Footer from './footer/Footer';
+import Content from './content/Content';
 
 
 
 export default class App extends Component {
 
-    state = {
-        data: [],
-        sliced: []
-    }
-
-    requestStockList = () => {
-        fetch(`https://financialmodelingprep.com/api/v3/company/stock/list`)
-            .then(res => res.json())
-            .then(res => {
-                console.log()
-                this.setState(
-                    {
-                        data: res.symbolsList,
-                        sliced: res.symbolsList.slice(0, 10)
-                    }
-                )
-            })
-    }
-
-    componentDidMount() {
-        this.requestStockList();
-    }
     render() {
-        console.log(this.state.data);
         return (
             <BrowserRouter>
                 <Header />
-                <div>
-                    <Route exact path='/' render={() => <div>Account</div>} />
-                    <Route path='/account' render={() => <div>Account</div>} />
-                    <Route path='/stock' render={() => {
-                        return <div>
-                            {
-                                this.state.sliced.map((item, index) => {
-                                    return <ul key={index}>
-                                        <li>{item.symbol}</li>
-                                        <li>{item.name}</li>
-                                        <li>{item.price}</li>
-                                        <li>{item.exchange}</li>
-                                    </ul>
-                                })
-                            }
-                        </div>
-                    }} />
-                </div>
+                <Content />
                 <Footer />
             </BrowserRouter>
         )
--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Account from "./account/Account";
+import styles from './Content.module.css';
 
-class Content extends React.Component{
+
+class Content extends React.Component {
     render() {
         return (
-            <div>
-                <Account />
+            <div className = { styles.wrapper }>
+                <Route exact path='/account' render={() => <Account />} />
+                {/* <Route path='/stock' render={() => <Stock />} />
+                <Route path='/buy' render={() => <Buy />} /> */}
             </div>
         );
     }
